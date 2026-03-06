1e97d8cc4ded714ce15320992dfae51c91d907e3
Vitalii Aleksashin <zenix@cn.ru>
2026-03-06 16:21:02 +0700

    remove python 3.7 support

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.19.0",
         "scikit-learn>=1.0.0",
